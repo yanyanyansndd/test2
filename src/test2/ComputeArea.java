bbcc5ad1ac0f1c0f39dbcc70893fcd4c95b9131d
yanyanyansndd <396987602@qq.com>
2021-10-08 23:26:45 +0800

    test2

---
+++ src/test2/ComputeArea.java
@@ -0,0 +1,21 @@
+package test2;
+
+public class ComputeArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double radius; // Declare radius
+        double area; //Declare area
+        
+        //Assign a radius
+        radius = 20; //radius is now 20
+        
+        //Compute area
+        area = radius * radius * 3.14159;
+        
+        //Display results
+        System.out.println("The area for the circle of radius " +
+          radius + " is " + area);
+	}
+
+}
